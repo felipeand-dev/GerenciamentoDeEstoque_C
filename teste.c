dc3614bf3f668158c5e88a99bf3d557cba312176
FELIPE ANDRADE <felipebaiano04@gmail.com>
2026-08-22 16:50:56 -0300

    iniciando projeto

---
+++ teste.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+  int select = 0;
+
+  printf("-----------------------------\n");
+  printf("MERCADO - PRECO BAIXO\n");
+  printf("-----------------------------\n");
+  printf("\n-------------MENU------------\n\n");
+  printf("1. Cadastrar produto\n");
+  printf("2. Remover produto por id\n");
+  printf("3. Listar todos os produtos\n");
+  printf("4. Buscar produto por id \n");
+  printf("5. Ordenar produtos por preço\n");
+  printf("\n-----------------------------\n");
+  printf("Digite o numero: ");
+  scanf("%d", &select);
+
+  switch (select)
+  {
+  case 1:
+    break;
+  case 2:
+    break;
+  case 3:
+    break;
+  case 4:
+    break;
+  case 5:
+    break;
+  default:
+    break;
+  }
+
+  return 0;
+}
